93064f3c2c8a76cb6bed0bd85ab53b10dfb82cff
xsadia <lipearthur81@gmail.com>
2022-02-22 17:28:22 -0300

    feat: change file structure && rename app.go to server.go

---
+++ api/server.go
@@ -0,0 +1,51 @@
+package api
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type Server struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *Server) Initialize(host, user, password, dbname string) {
+	connectionString :=
+		fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", host, user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	a.Router = mux.NewRouter()
+
+	a.initializeRoutes()
+}
+
+func (a *Server) Run(address string) {
+	log.Fatal(http.ListenAndServe(address, a.Router))
+}
+
+func (a *Server) initializeRoutes() {
+	a.Router.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		response := make(map[string]bool)
+
+		response["ok"] = true
+
+		w.Header().Set("Content-type", "application/json")
+		jsonResp, _ := json.Marshal(&response)
+
+		w.Write(jsonResp)
+
+	}).Methods("GET")
+}
