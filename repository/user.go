aba42d36cccf9ed73caab8f4c4f5a40954fadbe3
xsadia <felipe.rosa.arthur27@gmail.com>
2022-02-25 02:06:26 -0300

    feat: add hashPassword and checkEmailAvailability functions

--- repository/user.go
+++ repository/user.go
@@ -1,8 +1,32 @@
 package repository
 
+import (
+	"database/sql"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	Email        string `json:"email"`
 	Username     string `json:"username"`
 	Password     string `json:"password"`
 	RefreshToken string `json:"refresh_token"`
 }
+
+func hashPassword(password []byte, salt int) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword(password, salt)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
+
+func checkEMailAvailability(email string, db *sql.DB) (err error) {
+	return db.QueryRow("SELECT COUNT(*) FROM users WHERE users.email = $1", email).Scan(&err)
+}
+
+// func (u *User) Create(db *sql.DB) error {
+
+// }
