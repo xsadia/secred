fbb6c6491059143ed6b42e089ee2996ed392903e
xsadia <felipe.rosa.arthur27@gmail.com>
2022-02-26 15:39:52 -0300

    fix: add workaround on the .env problem in tests

--- config/env.go
+++ config/env.go
@@ -7,6 +7,15 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func GetEnvTest(key string) string {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return os.Getenv(key)
+}
+
 func GetEnv(key string) string {
 	err := godotenv.Load(".env")
 	if err != nil {
