8c448fada6b988b1dc4663eb06b991a87100f6cb
xsadia <felipe.rosa.arthur27@gmail.com>
2022-02-28 14:31:49 -0300

    refactor: refactor GetEnv function

--- config/env.go
+++ config/env.go
@@ -7,17 +7,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func GetEnvTest(key string) string {
-	err := godotenv.Load("../.env")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return os.Getenv(key)
-}
-
-func GetEnv(key string) string {
-	err := godotenv.Load(".env")
+func GetEnv(path, key string) string {
+	err := godotenv.Load(path)
 	if err != nil {
 		log.Fatal(err)
 	}
