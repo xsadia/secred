f7ca610227f459b8695bab15f3234c1abd0a67fc
xsadia <felipe.rosa.arthur27@gmail.com>
2022-02-28 01:40:46 -0300

    feat: add CreateToken function

---
+++ config/jwt.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func CreateToken(id string) (token string, err error) {
+
+	atClaims := jwt.MapClaims{}
+	atClaims["user_id"] = id
+	atClaims["exp"] = time.Now().Add(time.Hour * 9999).Unix()
+
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err = at.SignedString([]byte(GetEnv("JWT_ACCESS_SECRET")))
+
+	return token, err
+}
