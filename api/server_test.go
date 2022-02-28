f946bad50cff055483f3315e4ff062e84ebb6722
xsadia <felipe.rosa.arthur27@gmail.com>
2022-02-28 14:33:03 -0300

    feat: add user auth tests

--- api/server_test.go
+++ api/server_test.go
@@ -18,10 +18,10 @@ func TestMain(m *testing.M) {
 
 	s.InitializeRoutes()
 	s.InitializeDB(
-		config.GetEnvTest("APP_DB_HOST"),
-		config.GetEnvTest("APP_DB_USERNAME"),
-		config.GetEnvTest("APP_DB_PASSWORD"),
-		config.GetEnvTest("APP_DB_NAME"),
+		config.GetEnv("../.env", "APP_DB_HOST"),
+		config.GetEnv("../.env", "APP_DB_USERNAME"),
+		config.GetEnv("../.env", "APP_DB_PASSWORD"),
+		config.GetEnv("../.env", "APP_DB_NAME"),
 	)
 
 	code := m.Run()
@@ -62,6 +62,7 @@ func TestCreateUser(t *testing.T) {
 		executeRequest(r)
 
 		r, _ = http.NewRequest("POST", "/user", bytes.NewBuffer(jsonStr))
+		r.Header.Set("Content-Type", "application/json")
 		response := executeRequest(r)
 
 		checkResponseCode(t, 409, response.Code)
@@ -70,6 +71,68 @@ func TestCreateUser(t *testing.T) {
 
 		if m["error"] != emailAlreadyInUserError {
 			t.Errorf("expected '%v', got '%v'", emailAlreadyInUserError, m["error"])
+		}
+	})
+}
+
+func TestAuthUser(t *testing.T) {
+	clearTables()
+
+	var userCreationStr = []byte(`{
+		"email":"testuser@example.com", 
+		"username":"testUser",
+		"password":"123123"
+		}`)
+
+	r, _ := http.NewRequest("POST", "/user", bytes.NewBuffer(userCreationStr))
+	r.Header.Set("Content-Type", "application/json")
+
+	executeRequest(r)
+
+	t.Run("should return token if e-mail and password match", func(t *testing.T) {
+
+		var userAuthStr = []byte(`{
+			"email":"testuser@example.com", 
+			"password":"123123"
+			}`)
+
+		r, _ := http.NewRequest("POST", "/auth", bytes.NewBuffer(userAuthStr))
+		r.Header.Set("Content-Type", "application/json")
+
+		response := executeRequest(r)
+
+		checkResponseCode(t, 200, response.Code)
+
+		var m map[string]string
+
+		json.Unmarshal(response.Body.Bytes(), &m)
+
+		_, ok := m["token"]
+
+		if !ok {
+			t.Error("Expected token got nothing")
+		}
+	})
+
+	t.Run("should return error and http code 401 if password or e-mail don't match", func(t *testing.T) {
+
+		var userAuthStr = []byte(`{
+			"email":"testuser@example.com", 
+			"password":"123122"
+			}`)
+
+		r, _ := http.NewRequest("POST", "/auth", bytes.NewBuffer(userAuthStr))
+		r.Header.Set("Content-Type", "application/json")
+
+		response := executeRequest(r)
+
+		checkResponseCode(t, 401, response.Code)
+
+		var m map[string]string
+		json.Unmarshal(response.Body.Bytes(), &m)
+
+		if m["error"] != wrongEmailPasswordCombinationError {
+			t.Errorf("expected '%v', got '%v'", wrongEmailPasswordCombinationError, m["error"])
 		}
 	})
 }
