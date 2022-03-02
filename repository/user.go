360cca58548d3432472c59dd7947ab00610aec85
xsadia <felipe.rosa.arthur27@gmail.com>
2022-03-01 23:03:04 -0300

    feat: add GetUserById and Activate method to user

--- repository/user.go
+++ repository/user.go
@@ -10,13 +10,25 @@ type User struct {
 	Email        string         `json:"email"`
 	Username     string         `json:"username"`
 	Password     string         `json:"password"`
+	Activated    bool           `json:"activated"`
 	RefreshToken sql.NullString `json:"refresh_token"`
 }
 
 func (u *User) GetUserByEmail(db *sql.DB) error {
 	return db.QueryRow(
-		"SELECT * FROM users WHERE users.email = $1", u.Email,
-	).Scan(&u.Id, &u.Email, &u.Username, &u.Password, &u.RefreshToken)
+		"SELECT * FROM users WHERE users.email = $1",
+		u.Email).Scan(&u.Id, &u.Email, &u.Username, &u.Password, &u.RefreshToken, &u.Activated)
+}
+
+func (u *User) GetUserById(db *sql.DB) error {
+	return db.QueryRow("SELECT * FROM users WHERE users.id = $1",
+		u.Id).Scan(&u.Id, &u.Email, &u.Username, &u.Password, &u.RefreshToken, &u.Activated)
+}
+
+func (u *User) Activate(db *sql.DB) error {
+	_, err := db.Exec("UPDATE users SET activated = TRUE WHERE id = $1", u.Id)
+
+	return err
 }
 
 func (u *User) Create(db *sql.DB) error {
