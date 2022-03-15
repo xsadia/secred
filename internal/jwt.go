b0264884a5724bd7782863ac25b111e44a21b0e6
xsadia <felipe.rosa.arthur27@gmail.com>
2022-03-15 15:19:48 -0300

    feat: add VerifyToken function

--- internal/jwt.go
+++ internal/jwt.go
@@ -6,14 +6,31 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+var notSoSecret = "Karinne"
+
 func CreateToken(id string, exp time.Duration) (token string, err error) {
 
-	atClaims := jwt.MapClaims{}
-	atClaims["user_id"] = id
-	atClaims["exp"] = time.Now().Add(time.Hour * exp).Unix()
+	claims := jwt.MapClaims{}
+	claims["user_id"] = id
+	claims["exp"] = time.Now().Add(time.Hour * exp).Unix()
 
-	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	token, err = at.SignedString([]byte("Karinne"))
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err = at.SignedString([]byte(notSoSecret))
 
 	return token, err
+}
+
+func VerifyToken(token string) (jwt.MapClaims, error) {
+
+	claims := jwt.MapClaims{}
+
+	_, err := jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(notSoSecret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return claims, nil
 }
