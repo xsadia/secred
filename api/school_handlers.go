040ba2cb26bd666badec1acb382166028f26a894
xsadia <felipe.rosa.arthur27@gmail.com>
2022-04-21 23:44:20 -0300

    feat(school): add CreateSchool handler

---
+++ api/school_handlers.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/xsadia/secred/internal"
+	"github.com/xsadia/secred/repository"
+)
+
+func (s *Server) CreateSchoolHandler(w http.ResponseWriter, r *http.Request) {
+	ah := r.Header.Get("Authorization")
+
+	token, err := internal.ValidateAuthHeader(ah)
+
+	if err != nil {
+		internal.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	_, err = internal.VerifyToken(token)
+
+	if err != nil {
+		internal.RespondWithError(w, http.StatusUnauthorized, err.Error())
+		return
+	}
+
+	var sc repository.School
+
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&sc); err != nil {
+		internal.RespondWithError(w, http.StatusBadRequest, invalidRequestPayloadError)
+		return
+	}
+
+	if err = sc.CreateSchool(s.DB); err != nil {
+		internal.RespondWithError(w, http.StatusConflict, "School already registered")
+		return
+	}
+
+	internal.RespondWithJSON(w, http.StatusCreated, sc)
+}
