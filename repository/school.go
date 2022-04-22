454b888d432a9c717ec03c29670380c26d2bf155
xsadia <felipe.rosa.arthur27@gmail.com>
2022-04-21 23:38:53 -0300

    feat(school): add CreateSchool method

--- repository/school.go
+++ repository/school.go
@@ -1,6 +1,12 @@
 package repository
 
+import "database/sql"
+
 type School struct {
 	Id   string `json:"id"`
 	Name string `json:"name"`
+}
+
+func (s *School) CreateSchool(db *sql.DB) error {
+	return db.QueryRow("INSERT INTO schools (name) VALUES ($1) RETURNING id", s.Name).Scan(&s.Id)
 }
